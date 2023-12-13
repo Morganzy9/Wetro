391ff6974bc3c06e974ddfa020dd04ebb5847d9d
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-12-13 17:09:20 +0500

    Initial Commit

---
+++ Wetro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wetro
+//
+//  Created by Ислам Пулатов on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
