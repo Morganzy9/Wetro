6b5588ec0fbe29303ec6c854917c67b739a8ca3f
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2024-01-25 22:48:38 +0500

    Make Correction in Model

--- Wetro/Models/RecentlySongsModel.swift
+++ Wetro/Models/RecentlySongsModel.swift
@@ -88,32 +88,6 @@ struct Track: Codable {
     }
 }
 
-// MARK: - Album
-struct Album: Codable {
-    let albumType: String
-    let artists: [Artist]
-    let availableMarkets: [String]
-    let externalUrls: ExternalUrls
-    let href: String
-    let id: String
-    let images: [Image]
-    let name, releaseDate, releaseDatePrecision: String
-    let totalTracks: Int
-    let type, uri: String
-
-    enum CodingKeys: String, CodingKey {
-        case albumType = "album_type"
-        case artists
-        case availableMarkets = "available_markets"
-        case externalUrls = "external_urls"
-        case href, id, images, name
-        case releaseDate = "release_date"
-        case releaseDatePrecision = "release_date_precision"
-        case totalTracks = "total_tracks"
-        case type, uri
-    }
-}
-
 // MARK: - Artist
 struct Artist: Codable {
     let externalUrls: ExternalUrls
